b151d0b1ab81204b93a5cdb7085caa1bdfe9f9bd
Eddie Wang <eddie@eddiewang.me>
2017-03-08 08:56:46 -0500

    added prod settings

---
+++ webpack/webpack.prod.babel.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const cssnext = require('postcss-cssnext')
+const postcssFocus = require('postcss-focus')
+const postcssReporter = require('postcss-reporter')
+
+module.exports = require('./webpack.base.babel.js')({
+  entry: [
+    // disabled eventsource-polyfill because who the fuck uses IE to dev
+    'babel-polyfill',
+    path.join(process.cwd(), 'app/app.js')
+  ],
+
+  output: {
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  devtool: 'cheap-module-source-map',
+
+  cssLoader: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: 'css-loader!postcss-loader'
+  }),
+
+  lessLoader: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: 'css-loader!postcss-loader!less-loader'
+  }),
+
+  postcssPlugins: [
+    postcssFocus(),
+    cssnext({
+      browsers: ['last 2 versions', 'IE > 10']
+    }),
+    postcssReporter({
+      clearMessages: true
+    })
+  ],
+
+  babelQuery: {
+    presets: ['es2015', 'stage-0', 'react'],
+    plugins: ['transform-async-to-generator', 'transform-decorators-legacy']
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false // ...but do not show warnings in the console (there is a lot of them)
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
+    new ExtractTextPlugin('[name].[contenthash].css')
+  ]
+})
