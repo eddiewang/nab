49971069bb79603a249a67d498e6f50f8db0f8c3
Eddie Wang <eddie@eddiewang.me>
2017-03-25 01:43:09 -0400

    addedsome hackathon starter optz

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const bodyParser = require('body-parser')
 const myApi = require('./api')
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 app.use('/api', myApi)
 
