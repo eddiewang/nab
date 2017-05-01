5c5d8b0d1e278a9c30631f8925cad45e71440ef7
Eddie Wang <eddie@eddiewang.me>
2017-05-01 13:07:13 -0400

    latest

--- app/containers/Landing.js
+++ app/containers/Landing.js
@@ -1,37 +1,20 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import { inject, observer } from 'mobx-react'
+
+const Container = styled.div`
+  width: 400px;
+  margin: 0 auto;
+`
 
 @inject('mainStore')
 @observer
 class Landing extends React.Component {
-  componentDidMount () {
-    setInterval(() => { this.props.mainStore.counter += 1 }, 1000)
-  }
   render () {
     return (
-      <div className='container'>
-        <div className='card-container'>
-          <div className='section'>
-            <div className='header'>
-              <div className='title'>Custom App</div>
-            </div>
-          </div>
-          <div className='card'>
-            <div className='section'>
-              <div className='row-sm'>
-                <div className='header'>
-                  <div className='title'>This is a custom title</div>
-                </div>
-              </div>
-              <div className='buttons'>
-                <button className='button primary-action'>Primary</button>
-                <button className='button secondary-action'>Secondary</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <h1>let's get coding</h1>
+      </Container>
     )
   }
 }
