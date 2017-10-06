8d6fa09e9c24f0c31de61a258acd6081de26ba95
Eddie Wang <bonjour@eddiewang.me>
2017-10-06 14:19:36 -0400

    added procfile and dockerfile

--- webpack/webpack.base.babel.js
+++ webpack/webpack.base.babel.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = (options) => ({
+module.exports = options => ({
   entry: options.entry,
 
-  output: Object.assign({
-    path: path.resolve(process.cwd(), 'build'),
-    publicPath: '/'
-  }, options.output),
+  output: Object.assign(
+    {
+      path: path.resolve(process.cwd(), 'build'),
+      publicPath: '/'
+    },
+    options.output
+  ),
 
   module: {
     loaders: [
@@ -16,26 +19,33 @@ module.exports = (options) => ({
         test: /\.js$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
-      }, {
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      }, {
+      },
+      {
         test: /\.scss$/,
         loader: 'style-loader!css-loader!postcss-loader!sass-loader'
-      }, {
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: 'file-loader'
-      }, {
+      },
+      {
         test: /\.html$/,
         loader: 'html-loader'
-      }, {
+      },
+      {
         test: /\.json$/,
         loader: 'json-loader'
-      }, {
+      },
+      {
         test: /\.(eot|ttf|woff|woff2)$/,
         loader: 'file-loader'
-      }]
+      }
+    ]
   },
 
   plugins: options.plugins.concat([
@@ -53,16 +63,8 @@ module.exports = (options) => ({
 
   resolve: {
     modules: ['app', 'node_modules', 'app/assets', 'data'],
-    extensions: [
-      '.js',
-      '.json'
-    ],
-    mainFields: [
-      'browser',
-      'jsnext:main',
-      'module',
-      'main'
-    ]
+    extensions: ['.js', '.json'],
+    mainFields: ['browser', 'jsnext:main', 'module', 'main']
   },
   devtool: options.devtool
 })
