3a2ce96d334f10dff14df3128b1eb55f9bcaa391
Eddie Wang <bonjour@eddiewang.me>
2017-11-12 22:34:58 -0500

    added public folder

--- webpack/webpack.base.babel.js
+++ webpack/webpack.base.babel.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = options => ({
   entry: options.entry,
@@ -7,7 +8,7 @@ module.exports = options => ({
   output: Object.assign(
     {
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/'
+      publicPath: '/',
     },
     options.output
   ),
@@ -18,53 +19,54 @@ module.exports = options => ({
         enforce: 'pre',
         test: /\.js$/,
         loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader',
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: 'file-loader'
+        loaders: 'file-loader',
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: 'html-loader',
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader'
-      }
-    ]
+        loader: 'file-loader',
+      },
+    ],
   },
 
   plugins: options.plugins.concat([
     new webpack.ProvidePlugin({
-      fetch: 'exports-loader?'
+      fetch: 'exports-loader?',
     }),
+    new CopyWebpackPlugin([{ from: 'public' }]),
 
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
-    new webpack.NamedModulesPlugin()
+    new webpack.NamedModulesPlugin(),
   ]),
 
   resolve: {
     modules: ['app', 'node_modules', 'app/assets', 'data'],
     extensions: ['.js', '.json'],
-    mainFields: ['browser', 'jsnext:main', 'module', 'main']
+    mainFields: ['browser', 'jsnext:main', 'module', 'main'],
   },
-  devtool: options.devtool
+  devtool: options.devtool,
 })
